d2b56526e0f343dd4251a7e71489a01820d39b6d
Hiroki Kashimura <hiroki@gfd-dennou.org>
2019-02-21 14:12:21 +0900

    update dcl.rb to ver. 7.3.4

--- dcl.rb
+++ dcl.rb
@@ -1,12 +1,12 @@
 class Dcl < Formula
   desc "This is a Fortran version of DCL, a drawing library useful for planetary and Earth sciences. This library includes graphical, character processing, and mathematical libraries."
   homepage ""
-  url "https://www.gfd-dennou.org/library/dcl/dcl-7.1.2.tar.gz"
-  version "7.1.2"
-  sha256 "01400cc516698e915a51fc5a004fe7ac6c25e470a565fd49f4affed44f3975ca"
+  url "http://www.gfd-dennou.org/library/dcl/dcl-7.3.4.tar.gz"
+  version "7.3.4"
+  sha256 "5f38d8206f3f473321dca3717bc060921c5360ed5d47403b50677c847c439f93"
 
   depends_on 'gcc'
-  depends_on 'gtk+'
+  depends_on 'gtk+3'
   depends_on 'pkg-config'
 
   def install
