180c56e2af9a3f8364e54f404fee2b96325e50da
Hiroki Kashimura <hiroki@gfd-dennou.org>
2019-02-21 14:35:34 +0900

    bugfix

--- dcl.rb
+++ dcl.rb
@@ -11,7 +11,7 @@ class Dcl < Formula
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix} --with-gtk2"
+    system "./configure", "--prefix=#{prefix}", "--with-gtk2"
     system "make"
     system "make install"
   end
